a7cadd8e3b804db88f6daaa5c8c9c873fbd36943
J.A.R.V.I.S <creatorofjarvis@gmail.com>
2020-09-09 00:03:36 +0530

    Added

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -5,7 +5,7 @@ class Development(Config):
     OWNER_ID = 1365686472  # my telegram ID
     OWNER_USERNAME = "ramshouriesh"  # my telegram username
     API_KEY = "1334628285:AAHt_HbyiJv4y0ftPwJZL_tqjuCK1kKFUnU"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'mongodb+srv://Myuserbot:Myuserbot@cluster0.c275h.mongodb.net/<dbname>?retryWrites=true&w=majority'  # sample db credentials
+    SQLALCHEMY_DATABASE_URI = 'mongodb+srv://securitybot:securitybot@cluster0.tmdok.mongodb.net/SecurityBot?retryWrites=true&w=majority'  # sample db credentials
     MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [1318486004, 1203294851]  # List of id's for users which have sudo access to the bot.
